ec620e324747ac84b71317a7e8370f057ca8ff47
realbot <realbot@gmail.com>
2016-02-17 14:27:20 +0100

    added test for buckle update

--- src/buckle/data/data_test.go
+++ src/buckle/data/data_test.go
@@ -1,19 +1,42 @@
 package data
 
 import (
-	"testing"
+    "testing"
 )
 
-func TestBoh(t *testing.T) {
-	dataf := createTempBuckleDataFile()
-	WriteBuckleData(dataf, "foo", "8f24409843c176fa2c0b4690bfc94d15")
-	dataf.Close()
-	
-	data, err := ReadBuckleData(dataf.Name())
-	if err != nil {
-		t.Errorf("Error reading buckle data file: %v", err)
-	}
-	if data.Hashes["foo"] != "8f24409843c176fa2c0b4690bfc94d15" {
-		t.Errorf("Data mismatch: expected bar was %s", data.Hashes["foo"])
-	}
+func TestSaveAndReadBuckleData(t *testing.T) {
+    dataf := createTempBuckleDataFile()
+    WriteBuckleData(dataf, "foo", "8f24409843c176fa2c0b4690bfc94d15")
+    dataf.Close()
+
+    buckleData, err := ReadBuckleData(dataf.Name())
+
+    if err != nil {
+        t.Errorf("Error reading buckle data file: %v", err)
+    }
+    if buckleData.Hashes["foo"] != "8f24409843c176fa2c0b4690bfc94d15" {
+        t.Errorf("Data mismatch: expected 8f24409843c176fa2c0b4690bfc94d15 was %s", buckleData.Hashes["foo"])
+    }
+}
+
+func TestUpdateBuckleData(t *testing.T)  {
+    dataf := createTempBuckleDataFile()
+    WriteBuckleData(dataf, "foo", "8f24409843c176fa2c0b4690bfc94d15")
+    dataf.Close()
+    
+    buckleData, err := ReadBuckleData(dataf.Name())
+    if err != nil {
+        t.Errorf("Error reading buckle data file: %v", err)
+    }
+
+    buckleData.Hashes["foo"] = "12345678901234567890123456789012"
+    buckleData.UpdateBuckleData(dataf.Name())
+    reloadedData, err := ReadBuckleData(dataf.Name())
+
+    if err != nil {
+        t.Errorf("Error updating buckle data file: %v", err)
+    }
+    if reloadedData.Hashes["foo"] != "12345678901234567890123456789012" {
+        t.Errorf("Data was not updated: expected 12345678901234567890123456789012 was %s", reloadedData.Hashes["foo"])
+    }    
 }
